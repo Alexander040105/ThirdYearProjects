48d98c8abfdfc54ad4c0b0eedd4cf4807f8f57b8
Alexander040105 <alexanderjonsolis0401@gmail.com>
2025-08-04 22:47:11 +0800

    done na sa assignmenttt

---
+++ operating_systems/assignment1.bat
@@ -0,0 +1,14 @@
+cd \
+mkdir BSCS3109
+cd BSCS3109
+mkdir ALEXANDERJON
+mkdir SOLIS
+echo This is LABSAMPLE1 > LABSAMPLE1.TXT
+
+cd ALEXANDERJON
+echo Placeholder > labsample2.doc
+
+cd ..
+cd SOLIS
+echo Placeholder > labsample3.xls
+
