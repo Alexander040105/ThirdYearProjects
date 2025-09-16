11d82da54e0c809817c7435e74163b31df7ce504
Alexander040105 <alexanderjonsolis05@gmail.com>
2025-09-16 20:48:15 +0800

    commit new files from the laptop

--- information_security/md5.py
+++ information_security/md5.py
@@ -1,12 +1,12 @@
 import hashlib
 
 def main():
-        text = input("Enter text to generate MD5 hash: ")
-        if not text.strip():
-                print("No text provided. Please try again.")
-                return
-        md5_hash = hashlib.md5(text.encode()).hexdigest()
-        print(f'\nMD5 Hash: {md5_hash}')
+    file_path = input('Enter file path to generate MD5 hash: ')
+    with open(f'{file_path}', 'r') as file:
+        content = file.read()
+    md5_hash = hashlib.md5(content.encode()).hexdigest()
+    print(f'\nThe original text: {content}')
+    print(f'\nMD5 Hash: {md5_hash}')
 
 if __name__ == '__main__':
-        main()
+    main()
