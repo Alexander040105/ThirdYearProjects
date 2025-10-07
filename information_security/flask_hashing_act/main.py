2b1e665b93dffc436f34a7d0937effe3adf51a83
Alexander040105 <alexanderjonsolis05@gmail.com>
2025-10-07 22:27:57 +0800

    hello.pyForMidterms

--- information_security/flask_hashing_act/main.py
+++ information_security/flask_hashing_act/main.py
@@ -51,4 +51,4 @@ def home():
     return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=False)
+    app.run(host="172.16.0.29", port=8080, debug=False)
